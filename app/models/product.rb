1cdd446da8edeae75f2d1e1894defdba035a4ab8
Bruno Alves <bsouza@korewa.com.br>
2013-08-08 16:02:27 -0300

    redimensionamento de imagens

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ActiveRecord::Base
   #attr_accessible :summary, :body, :link, :active, :published
   belongs_to :subcategory
   has_many :product_images
-  has_attached_file:logo, :styles =>{:thumb => "50x50#",:logo => "291x138#"}
+  has_attached_file:logo, :styles =>{:thumb => "50x50>",:logo => "291x138>"}
 end
